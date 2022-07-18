7c18320285035be2c030e5822b011c0983810bce
djennedy <darren_jennedy@sfu,ca>
2022-07-17 21:41:38 -0700

    Implemented basic game loop structure

--- src/GameController.java
+++ src/GameController.java
@@ -1,9 +1,20 @@
 import javax.swing.JComponent;
 import java.awt.Graphics2D;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GameController extends JComponent {
+import static java.lang.System.currentTimeMillis;
+
+public class GameController extends JComponent implements KeyListener {
     private static GameBoard gameBoard;
+    private final int FRAMES_PER_SECOND = 2;
+    private final long FRAME_DURATION = 1000/FRAMES_PER_SECOND;
+    private boolean quitNextUpdate = false;
+
+    private void quit() {
+        quitNextUpdate = true;
+    }
 
     public GameController() {
         gameBoard = new GameBoard();
@@ -16,7 +27,25 @@ public class GameController extends JComponent {
     }
 
     public void start(){
+        long gameStartTime = System.currentTimeMillis();
         repaint();
+
+        while(!quitNextUpdate)
+        {
+            long elapsedTime = System.currentTimeMillis() - gameStartTime;
+            updateGame();
+            repaint();
+            elapsedTime += FRAME_DURATION;
+            long sleepDuration = elapsedTime - System.currentTimeMillis();
+            try
+            {
+                Thread.sleep(sleepDuration);
+            }
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     /**
@@ -27,5 +56,54 @@ public class GameController extends JComponent {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         gameBoard.draw(g2);
+    }
+
+    public void updateGame()
+    {
+        // TODO: Update game here
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        Direction direction;
+        switch (e.getKeyCode()) {
+            default:
+                return;
+
+            case KeyEvent.VK_Q:
+            case KeyEvent.VK_ESCAPE:
+                quit();
+                return;
+
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_A:
+                direction = Direction.West;
+                break;
+
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_W:
+                direction = Direction.North;
+                break;
+
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_D:
+                direction = Direction.East;
+                break;
+
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_S:
+                direction = Direction.South;
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
